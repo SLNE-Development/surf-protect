3d765b8c3d6b748d1d5b7f888d89c07950c3aee3
Simon <ammo@silence.ovh>
2023-07-14 16:48:42 +0200

    fix: Fixed hard bug

--- src/main/java/dev/slne/protect/bukkit/listener/listeners/ProtectionModeListener.java
+++ src/main/java/dev/slne/protect/bukkit/listener/listeners/ProtectionModeListener.java
@@ -1,5 +1,11 @@
 package dev.slne.protect.bukkit.listener.listeners;
 
+import dev.slne.protect.bukkit.BukkitMain;
+import dev.slne.protect.bukkit.message.MessageManager;
+import dev.slne.protect.bukkit.region.ProtectionRegion;
+import dev.slne.protect.bukkit.region.settings.ProtectionSettings;
+import dev.slne.protect.bukkit.user.ProtectionUser;
+import io.papermc.paper.event.player.PrePlayerAttackEntityEvent;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -17,158 +23,149 @@ import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.util.Vector;
 
-import dev.slne.protect.bukkit.BukkitMain;
-import dev.slne.protect.bukkit.message.MessageManager;
-import dev.slne.protect.bukkit.region.ProtectionRegion;
-import dev.slne.protect.bukkit.region.settings.ProtectionSettings;
-import dev.slne.protect.bukkit.user.ProtectionUser;
-import io.papermc.paper.event.player.PrePlayerAttackEntityEvent;
-
 public class ProtectionModeListener implements Listener {
 
-	@EventHandler
-	public void onQuit(PlayerQuitEvent event) {
-		ProtectionUser protectionUser = ProtectionUser.getProtectionUser(event.getPlayer());
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onQuit(PlayerQuitEvent event) {
+        ProtectionUser protectionUser = ProtectionUser.getProtectionUser(event.getPlayer());
 
-		if (protectionUser.hasRegionCreation()) {
-			protectionUser.getRegionCreation().cancelProtection();
-			protectionUser.resetRegionCreation();
-		}
-	}
+        if (protectionUser.hasRegionCreation()) {
+            protectionUser.getRegionCreation().cancelProtection();
+            protectionUser.resetRegionCreation();
+        }
+    }
 
-	@EventHandler
-	public void onMove(PlayerMoveEvent event) {
-		ProtectionUser protectionUser = ProtectionUser.getProtectionUser(event.getPlayer());
-		ProtectionRegion regionCreation = protectionUser.getRegionCreation();
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        ProtectionUser protectionUser = ProtectionUser.getProtectionUser(event.getPlayer());
+        ProtectionRegion regionCreation = protectionUser.getRegionCreation();
 
-		if (regionCreation != null) {
-			Player player = event.getPlayer();
-			Location protectionModeLocation = regionCreation.getStartLocation();
+        if (regionCreation != null) {
+            Player player = event.getPlayer();
+            Location protectionModeLocation = regionCreation.getStartLocation();
 
-			throwbackPlayer(player, protectionModeLocation);
-		}
-	}
+            throwbackPlayer(player, protectionModeLocation);
+        }
+    }
 
-	@EventHandler
-	public void onItemDrop(PlayerDropItemEvent event) {
-		ProtectionUser protectionUser = ProtectionUser.getProtectionUser(event.getPlayer());
-		ProtectionRegion regionCreation = protectionUser.getRegionCreation();
+    /**
+     * Throwback the player with a given force
+     */
+    private void throwbackPlayer(Player player, Location protectionModeLocation) {
+        int maxRange = ProtectionSettings.MAX_DISTANCE_FROM_PROTECTION_START;
+        int teleportMaxRange = ProtectionSettings.MAX_DISTANCE_FROM_PROTECTION_START_TELEPORT;
+        double throwbackForce = ProtectionSettings.MAX_DISTANCE_FROM_PROTECTION_START_FORCE;
 
-		if (regionCreation != null) {
-			event.setCancelled(true);
-		}
-	}
+        Location playerLocation = player.getLocation();
+        Location playerLocationYZero = playerLocation.clone();
+        playerLocationYZero.setY(0); // Ignore Y
+        Location protectionModeLocationYZero = protectionModeLocation.clone();
+        protectionModeLocationYZero.setY(0); // Ignore Y
 
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPortal(PlayerPortalEvent event) {
-		ProtectionUser protectionUser = ProtectionUser.getProtectionUser(event.getPlayer());
-		ProtectionRegion regionCreation = protectionUser.getRegionCreation();
+        Vector playerLocationVector = playerLocation.toVector();
+        Vector protectionModeLocationVector = protectionModeLocation.toVector();
+        Vector throwbackVector =
+                protectionModeLocationVector.subtract(playerLocationVector).multiply(throwbackForce).setY(0);
 
-		if (regionCreation != null || protectionUser.hasRegionCreation()) {
-			event.setCancelled(true);
-		}
-	}
+        double currentDistance = playerLocationYZero.distanceSquared(protectionModeLocationYZero);
 
-	@EventHandler
-	public void onDamage(EntityDamageEvent event) {
-		if (!(event.getEntity() instanceof Player)) {
-			return;
-		}
+        if (currentDistance >= maxRange && currentDistance < teleportMaxRange) {
+            player.setVelocity(throwbackVector);
+        } else if (currentDistance >= teleportMaxRange) {
+            player.teleport(protectionModeLocation);
+        } else {
+            return;
+        }
 
-		Player player = (Player) event.getEntity();
-		ProtectionUser protectionUser = ProtectionUser.getProtectionUser(player);
-		ProtectionRegion regionCreation = protectionUser.getRegionCreation();
+        player.sendMessage(MessageManager.getTooFarAwayFromStartComponent());
+        player.playSound(playerLocation, Sound.ENTITY_ENDER_DRAGON_FLAP, .75f, 2);
+    }
 
-		if (regionCreation != null) {
-			event.setCancelled(true);
-		}
-	}
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent event) {
+        ProtectionUser protectionUser = ProtectionUser.getProtectionUser(event.getPlayer());
+        ProtectionRegion regionCreation = protectionUser.getRegionCreation();
 
-	@EventHandler
-	public void onItemPickup(PlayerAttemptPickupItemEvent event) {
-		ProtectionUser protectionUser = ProtectionUser.getProtectionUser(event.getPlayer());
-		ProtectionRegion regionCreation = protectionUser.getRegionCreation();
+        if (regionCreation != null) {
+            event.setCancelled(true);
+        }
+    }
 
-		if (regionCreation != null) {
-			event.setCancelled(true);
-		}
-	}
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPortal(PlayerPortalEvent event) {
+        ProtectionUser protectionUser = ProtectionUser.getProtectionUser(event.getPlayer());
+        ProtectionRegion regionCreation = protectionUser.getRegionCreation();
 
-	@EventHandler
-	public void onHandSwap(PlayerSwapHandItemsEvent event) {
-		ProtectionUser protectionUser = ProtectionUser.getProtectionUser(event.getPlayer());
-		ProtectionRegion regionCreation = protectionUser.getRegionCreation();
+        if (regionCreation != null || protectionUser.hasRegionCreation()) {
+            event.setCancelled(true);
+        }
+    }
 
-		if (regionCreation != null) {
-			event.setCancelled(true);
-		}
-	}
+    @EventHandler
+    public void onDamage(EntityDamageEvent event) {
+        if (!(event.getEntity() instanceof Player player)) {
+            return;
+        }
 
-	@EventHandler
-	public void onEntityAttack(PrePlayerAttackEntityEvent event) {
-		Player player = event.getPlayer();
-		ProtectionUser protectionUser = ProtectionUser.getProtectionUser(player);
-		ProtectionRegion regionCreation = protectionUser.getRegionCreation();
+        ProtectionUser protectionUser = ProtectionUser.getProtectionUser(player);
+        ProtectionRegion regionCreation = protectionUser.getRegionCreation();
 
-		if (regionCreation != null) {
-			event.setCancelled(true);
-		}
-	}
+        if (regionCreation != null) {
+            event.setCancelled(true);
+        }
+    }
 
-	@EventHandler
-	public void onEntityTarget(EntityTargetEvent event) {
-		if (!(event.getTarget() instanceof Player)) {
-			return;
-		}
+    @EventHandler
+    public void onItemPickup(PlayerAttemptPickupItemEvent event) {
+        ProtectionUser protectionUser = ProtectionUser.getProtectionUser(event.getPlayer());
+        ProtectionRegion regionCreation = protectionUser.getRegionCreation();
 
-		Player player = (Player) event.getTarget();
-		ProtectionUser protectionUser = ProtectionUser.getProtectionUser(player);
-		ProtectionRegion regionCreation = protectionUser.getRegionCreation();
+        if (regionCreation != null) {
+            event.setCancelled(true);
+        }
+    }
 
-		if (regionCreation != null) {
-			event.setCancelled(true);
-		}
-	}
+    @EventHandler
+    public void onHandSwap(PlayerSwapHandItemsEvent event) {
+        ProtectionUser protectionUser = ProtectionUser.getProtectionUser(event.getPlayer());
+        ProtectionRegion regionCreation = protectionUser.getRegionCreation();
 
-	@EventHandler
-	public void onServerStop(PluginDisableEvent event) {
-		for (ProtectionUser protectionUser : BukkitMain.getBukkitInstance().getUserManager().getUsers()) {
-			if (protectionUser.hasRegionCreation()) {
-				protectionUser.getRegionCreation().cancelProtection();
-			}
-		}
-	}
+        if (regionCreation != null) {
+            event.setCancelled(true);
+        }
+    }
 
-	/**
-	 * Throwback the player with a given force
-	 */
-	private void throwbackPlayer(Player player, Location protectionModeLocation) {
-		int maxRange = ProtectionSettings.MAX_DISTANCE_FROM_PROTECTION_START;
-		int teleportMaxRange = ProtectionSettings.MAX_DISTANCE_FROM_PROTECTION_START_TELEPORT;
-		double throwbackForce = ProtectionSettings.MAX_DISTANCE_FROM_PROTECTION_START_FORCE;
+    @EventHandler
+    public void onEntityAttack(PrePlayerAttackEntityEvent event) {
+        Player player = event.getPlayer();
+        ProtectionUser protectionUser = ProtectionUser.getProtectionUser(player);
+        ProtectionRegion regionCreation = protectionUser.getRegionCreation();
 
-		Location playerLocation = player.getLocation();
-		Location playerLocationYZero = playerLocation.clone();
-		playerLocationYZero.setY(0); // Ignore Y
-		Location protectionModeLocationYZero = protectionModeLocation.clone();
-		protectionModeLocationYZero.setY(0); // Ignore Y
+        if (regionCreation != null) {
+            event.setCancelled(true);
+        }
+    }
 
-		Vector playerLocationVector = playerLocation.toVector();
-		Vector protectionModeLocationVector = protectionModeLocation.toVector();
-		Vector throwbackVector = protectionModeLocationVector.subtract(playerLocationVector).multiply(
-				throwbackForce).setY(0);
+    @EventHandler
+    public void onEntityTarget(EntityTargetEvent event) {
+        if (!(event.getTarget() instanceof Player player)) {
+            return;
+        }
 
-		double currentDistance = playerLocationYZero.distanceSquared(protectionModeLocationYZero);
+        ProtectionUser protectionUser = ProtectionUser.getProtectionUser(player);
+        ProtectionRegion regionCreation = protectionUser.getRegionCreation();
 
-		if (currentDistance >= maxRange && currentDistance < teleportMaxRange) {
-			player.setVelocity(throwbackVector);
-		} else if (currentDistance >= teleportMaxRange) {
-			player.teleport(protectionModeLocation);
-		} else {
-			return;
-		}
+        if (regionCreation != null) {
+            event.setCancelled(true);
+        }
+    }
 
-		player.sendMessage(MessageManager.getTooFarAwayFromStartComponent());
-		player.playSound(playerLocation, Sound.ENTITY_ENDER_DRAGON_FLAP, .75f, 2);
-	}
+    @EventHandler
+    public void onServerStop(PluginDisableEvent event) {
+        for (ProtectionUser protectionUser : BukkitMain.getBukkitInstance().getUserManager().getUsers()) {
+            if (protectionUser.hasRegionCreation()) {
+                protectionUser.getRegionCreation().cancelProtection();
+            }
+        }
+    }
 }
