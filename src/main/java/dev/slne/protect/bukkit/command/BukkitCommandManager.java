85b7afa216d3359eed40ffb29a23c2b603818dc3
Simon <ammo@silence.ovh>
2023-07-27 11:14:01 +0200

    fix: Removed old protection command

--- src/main/java/dev/slne/protect/bukkit/command/BukkitCommandManager.java
+++ src/main/java/dev/slne/protect/bukkit/command/BukkitCommandManager.java
@@ -1,6 +1,5 @@
 package dev.slne.protect.bukkit.command;
 
-import dev.slne.protect.bukkit.command.commands.ProtectionOldCommand;
 import dev.slne.protect.bukkit.command.commands.ProtectionWhoCommand;
 import dev.slne.protect.bukkit.command.commands.protection.ProtectionCommand;
 
@@ -11,7 +10,6 @@ public class BukkitCommandManager {
      */
     public void registerCommands() {
         new ProtectionCommand();
-        new ProtectionOldCommand();
         new ProtectionWhoCommand();
     }
 
