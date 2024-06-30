74c69ecc74323425574baf8c847ff5f9ccca300d
Ammo <ammo@slne.dev>
2024-07-01 00:48:58 +0200

    feat: Updated to 1.21
    Everything should be working as expected. Due to an error in IF, which I cannot resolve, I cannot test the plugin ingame

---
+++ src/main/java/dev/slne/protect/bukkit/gui/chest/SurfChestGui.java
@@ -0,0 +1,62 @@
+package dev.slne.protect.bukkit.gui.chest;
+
+import com.github.stefvanschie.inventoryframework.adventuresupport.ComponentHolder;
+import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
+import com.github.stefvanschie.inventoryframework.gui.type.util.NamedGui;
+import dev.slne.protect.bukkit.gui.SurfGui;
+import dev.slne.protect.bukkit.gui.utils.GuiUtils;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class SurfChestGui extends ChestGui implements SurfGui {
+
+  private final SurfGui parent;
+
+  protected SurfChestGui(@Nullable SurfGui parent, int rows, @NotNull Component title) {
+    this(parent, rows, title, true, true, true, true);
+  }
+
+  /**
+   * Creates a new chest gui.
+   *
+   * @param parent            the parent gui
+   * @param rows              the amount of rows this gui should have
+   * @param title             the title of this gui
+   * @param cancelBottomClick whether to cancel bottom click
+   * @param cancelBottomDrag  whether to cancel bottom drag
+   * @param cancelTopClick    whether to cancel top click
+   * @param cancelTopDrag     whether to cancel top drag
+   */
+  protected SurfChestGui(@Nullable SurfGui parent, int rows, @NotNull Component title,
+      boolean cancelBottomClick,
+      boolean cancelBottomDrag, boolean cancelTopClick, boolean cancelTopDrag) {
+    super(rows, ComponentHolder.of(title.colorIfAbsent(NamedTextColor.BLACK)));
+
+    this.parent = parent;
+
+    if (rows < 2) {
+      throw new IllegalArgumentException("Rows must be at least 2");
+    }
+
+    setOnBottomClick(event -> event.setCancelled(cancelBottomClick));
+    setOnBottomDrag(event -> event.setCancelled(cancelBottomDrag));
+    setOnTopClick(event -> event.setCancelled(cancelTopClick));
+    setOnTopDrag(event -> event.setCancelled(cancelTopDrag));
+
+    addPane(GuiUtils.getOutline(0));
+    addPane(GuiUtils.getOutline(rows - 1));
+    addPane(GuiUtils.getNavigation(this, rows - 1));
+  }
+
+  @Override
+  public @Nullable SurfGui getParent() {
+    return parent;
+  }
+
+  @Override
+  public @NotNull NamedGui getGui() {
+    return this;
+  }
+}
--- src/main/java/dev/slne/protect/bukkit/gui/list/ProtectionListGui.java
+++ src/main/java/dev/slne/protect/bukkit/gui/list/ProtectionListGui.java
@@ -4,14 +4,17 @@ import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.pane.PaginatedPane;
 import com.github.stefvanschie.inventoryframework.pane.StaticPane;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
-import dev.slne.gui.api.SurfGui;
-import dev.slne.gui.api.chest.SurfChestGui;
-import dev.slne.gui.api.utils.ItemUtils;
-import dev.slne.gui.api.utils.pagination.PageController;
+import dev.slne.protect.bukkit.gui.SurfGui;
+import dev.slne.protect.bukkit.gui.chest.SurfChestGui;
 import dev.slne.protect.bukkit.gui.protection.ProtectionShowGui;
+import dev.slne.protect.bukkit.gui.utils.ItemUtils;
+import dev.slne.protect.bukkit.gui.utils.pagination.PageController;
 import dev.slne.protect.bukkit.message.MessageManager;
 import dev.slne.protect.bukkit.region.ProtectionUtils;
 import dev.slne.protect.bukkit.region.info.RegionInfo;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Location;
@@ -19,133 +22,133 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 public class ProtectionListGui extends SurfChestGui {
 
-    private final StaticPane navigationPane;
-    private final PaginatedPane paginatedPane;
-    private final Map<World, List<ProtectedRegion>> regions;
+  private final StaticPane navigationPane;
+  private final PaginatedPane paginatedPane;
+  private final Map<World, List<ProtectedRegion>> regions;
 
-    private final Player viewingPlayer;
+  private final Player viewingPlayer;
 
-    /**
-     * Creates a new protection list gui
-     *
-     * @param parent        the parent gui
-     * @param regions       the regions to show
-     * @param viewingPlayer the player viewing the gui
-     */
-    @SuppressWarnings("java:S3776")
-    public ProtectionListGui(SurfGui parent, Map<World, List<ProtectedRegion>> regions, Player viewingPlayer) {
-        super(parent, 5, Component.text("Protections - Meine Grundstücke"));
+  /**
+   * Creates a new protection list gui
+   *
+   * @param parent        the parent gui
+   * @param regions       the regions to show
+   * @param viewingPlayer the player viewing the gui
+   */
+  @SuppressWarnings("java:S3776")
+  public ProtectionListGui(SurfGui parent, Map<World, List<ProtectedRegion>> regions,
+      Player viewingPlayer) {
+    super(parent, 5, Component.text("Protections - Meine Grundstücke"));
 
-        this.viewingPlayer = viewingPlayer;
+    this.viewingPlayer = viewingPlayer;
 
-        paginatedPane = new PaginatedPane(0, 1, 9, 3);
-        navigationPane = new StaticPane(0, 4, 9, 1);
+    paginatedPane = new PaginatedPane(0, 1, 9, 3);
+    navigationPane = new StaticPane(0, 4, 9, 1);
 
-        addPane(paginatedPane);
-        addPane(navigationPane);
+    addPane(paginatedPane);
+    addPane(navigationPane);
 
-        this.regions = regions;
+    this.regions = regions;
 
-        update();
-    }
+    update();
+  }
 
-    @Override
-    public void update() {
-        List<GuiItem> buttons = new ArrayList<>();
+  @Override
+  public void update() {
+    List<GuiItem> buttons = new ArrayList<>();
 
-        for (Map.Entry<World, List<ProtectedRegion>> entry : regions.entrySet()) {
-            World world = entry.getKey();
-            List<ProtectedRegion> worldRegions = entry.getValue();
+    for (Map.Entry<World, List<ProtectedRegion>> entry : regions.entrySet()) {
+      World world = entry.getKey();
+      List<ProtectedRegion> worldRegions = entry.getValue();
 
-            for (ProtectedRegion region : worldRegions) {
-                List<String> ownerNames = ProtectionUtils.getOwnerNames(region);
-                List<String> memberNames = ProtectionUtils.getMemberNames(region);
+      for (ProtectedRegion region : worldRegions) {
+        List<String> ownerNames = ProtectionUtils.getOwnerNames(region);
+        List<String> memberNames = ProtectionUtils.getMemberNames(region);
 
-                RegionInfo regionInfo = new RegionInfo(world, region);
-                Component none = Component.text("Keine", MessageManager.VARIABLE_VALUE);
+        RegionInfo regionInfo = new RegionInfo(world, region);
+        Component none = Component.text("Keine", MessageManager.VARIABLE_VALUE);
 
-                long distance = -1;
-                Location teleportLocation = regionInfo.getTeleportLocation();
-                if (teleportLocation != null) {
-                    distance =
-                            viewingPlayer.getWorld().equals(teleportLocation.getWorld()) ?
-                                    (long) viewingPlayer.getLocation().distance(regionInfo.getTeleportLocation()) : -1;
-                }
-
-                long area = regionInfo.getArea();
-
-                List<Component> lore = new ArrayList<>();
-                lore.add(Component.empty());
-                lore.add(Component.text("Entfernung: ", NamedTextColor.GRAY));
-                lore.add(Component.text(distance == -1 ? "Unbekannt" : distance + " Blöcke",
-                        MessageManager.VARIABLE_VALUE));
-                lore.add(Component.empty());
-                lore.add(Component.text("Größe: ", NamedTextColor.GRAY));
-                lore.add(Component.text(area + " Blöcke", MessageManager.VARIABLE_VALUE));
-                lore.add(Component.empty());
-
-                lore.add(Component.text("Besitzer:", NamedTextColor.GRAY));
-                if (ownerNames.isEmpty()) {
-                    lore.add(none);
-                } else {
-                    lore.addAll(ItemUtils.splitComponent(String.join(", ", ownerNames), 50,
-                            MessageManager.VARIABLE_VALUE));
-                }
-                lore.add(Component.empty());
-
-                lore.add(Component.text("Mitglieder:", NamedTextColor.GRAY));
-                if (memberNames.isEmpty()) {
-                    lore.add(none);
-                } else {
-                    lore.addAll(ItemUtils.splitComponent(String.join(", ", memberNames), 50,
-                            MessageManager.VARIABLE_VALUE));
-                }
-
-                lore.add(Component.empty());
-
-                final long finalDistance = distance;
-
-                buttons.add(new GuiItem(
-                        ItemUtils.item(Material.DIRT, 1, 0,
-                                Component.text(regionInfo.getName(), MessageManager.PRIMARY),
-                                lore.toArray(Component[]::new)), event -> {
-                    ProtectionShowGui oneGui =
-                            new ProtectionShowGui(this, region, area, finalDistance,
-                                    regionInfo, (Player) event.getWhoClicked());
-                    oneGui.show(event.getWhoClicked());
-                }));
-
-                paginatedPane.populateWithGuiItems(buttons);
-            }
+        long distance = -1;
+        Location teleportLocation = regionInfo.getTeleportLocation();
+        if (teleportLocation != null) {
+          distance =
+              viewingPlayer.getWorld().equals(teleportLocation.getWorld()) ?
+                  (long) viewingPlayer.getLocation().distance(regionInfo.getTeleportLocation())
+                  : -1;
         }
 
-        updateNavigation();
-        super.update();
+        long area = regionInfo.getArea();
+
+        List<Component> lore = new ArrayList<>();
+        lore.add(Component.empty());
+        lore.add(Component.text("Entfernung: ", NamedTextColor.GRAY));
+        lore.add(Component.text(distance == -1 ? "Unbekannt" : distance + " Blöcke",
+            MessageManager.VARIABLE_VALUE));
+        lore.add(Component.empty());
+        lore.add(Component.text("Größe: ", NamedTextColor.GRAY));
+        lore.add(Component.text(area + " Blöcke", MessageManager.VARIABLE_VALUE));
+        lore.add(Component.empty());
+
+        lore.add(Component.text("Besitzer:", NamedTextColor.GRAY));
+        if (ownerNames.isEmpty()) {
+          lore.add(none);
+        } else {
+          lore.addAll(ItemUtils.splitComponent(String.join(", ", ownerNames), 50,
+              MessageManager.VARIABLE_VALUE));
+        }
+        lore.add(Component.empty());
+
+        lore.add(Component.text("Mitglieder:", NamedTextColor.GRAY));
+        if (memberNames.isEmpty()) {
+          lore.add(none);
+        } else {
+          lore.addAll(ItemUtils.splitComponent(String.join(", ", memberNames), 50,
+              MessageManager.VARIABLE_VALUE));
+        }
+
+        lore.add(Component.empty());
+
+        final long finalDistance = distance;
+
+        buttons.add(new GuiItem(
+            ItemUtils.item(Material.DIRT, 1, 0,
+                Component.text(regionInfo.getName(), MessageManager.PRIMARY),
+                lore.toArray(Component[]::new)), event -> {
+          ProtectionShowGui oneGui =
+              new ProtectionShowGui(this, region, area, finalDistance,
+                  regionInfo, (Player) event.getWhoClicked());
+          oneGui.show(event.getWhoClicked());
+        }));
+
+        paginatedPane.populateWithGuiItems(buttons);
+      }
     }
 
-    /**
-     * Updates the navigation pane
-     */
-    private void updateNavigation() {
-        navigationPane.addItem(
-                PageController.PREVIOUS.toGuiItem(this, Component.text("Zurück", MessageManager.PRIMARY), paginatedPane,
-                        ItemUtils.paneItem()), 0, 0);
-        navigationPane.addItem(
-                PageController.NEXT.toGuiItem(this, Component.text("Weiter", MessageManager.PRIMARY), paginatedPane,
-                        ItemUtils.paneItem()), 8, 0);
-    }
+    updateNavigation();
+    super.update();
+  }
 
-    /**
-     * @return the regions
-     */
-    public Map<World, List<ProtectedRegion>> getRegions() {
-        return regions;
-    }
+  /**
+   * Updates the navigation pane
+   */
+  private void updateNavigation() {
+    navigationPane.addItem(
+        PageController.PREVIOUS.toGuiItem(this, Component.text("Zurück", MessageManager.PRIMARY),
+            paginatedPane,
+            ItemUtils.paneItem()), 0, 0);
+    navigationPane.addItem(
+        PageController.NEXT.toGuiItem(this, Component.text("Weiter", MessageManager.PRIMARY),
+            paginatedPane,
+            ItemUtils.paneItem()), 8, 0);
+  }
+
+  /**
+   * @return the regions
+   */
+  public Map<World, List<ProtectedRegion>> getRegions() {
+    return regions;
+  }
 
 }
