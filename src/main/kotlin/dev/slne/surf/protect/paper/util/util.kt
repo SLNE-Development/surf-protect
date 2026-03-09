f0a46b463970630e923f456d1ef7eddd188946de
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-03-09 16:29:51 +0100

    feat(protection): remove unused functions and clean up util.kt

--- src/main/kotlin/dev/slne/surf/protect/paper/util/util.kt
+++ src/main/kotlin/dev/slne/surf/protect/paper/util/util.kt
@@ -5,21 +5,16 @@ package dev.slne.surf.protect.paper.util
 import com.sk89q.worldedit.bukkit.BukkitAdapter
 import com.sk89q.worldedit.math.BlockVector2
 import com.sk89q.worldedit.math.BlockVector3
-import com.sk89q.worldedit.math.Vector2
-import com.sk89q.worldedit.regions.Region
 import com.sk89q.worldguard.LocalPlayer
 import com.sk89q.worldguard.WorldGuard
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin
 import com.sk89q.worldguard.protection.managers.RegionManager
 import com.sk89q.worldguard.protection.regions.ProtectedRegion
 import com.sk89q.worldguard.protection.regions.RegionContainer
-import com.sk89q.worldguard.protection.util.WorldEditRegionConverter
 import dev.slne.surf.protect.paper.region.flags.ProtectionFlagsRegistry
 import dev.slne.surf.surfapi.core.api.util.mutableObjectSetOf
 import dev.slne.surf.surfapi.core.api.util.toObjectList
-import io.papermc.paper.math.BlockPosition
 import io.papermc.paper.math.Position
-import kotlinx.coroutines.future.await
 import org.bukkit.Bukkit
 import org.bukkit.ChunkSnapshot
 import org.bukkit.Location
@@ -32,26 +27,6 @@ import com.sk89q.worldedit.util.Location as WorldEditLocation
 import com.sk89q.worldedit.world.World as WorldEditWorld
 
 /**
- * Converts a [ProtectedRegion] to a [Region] if possible.
- *
- * This function attempts to convert the instance of [ProtectedRegion]
- * into a [Region] using the WorldEditRegionConverter. If the conversion
- * is not possible, it returns null.
- *
- * @return The converted [Region] instance if successful, or null if the conversion fails.
- */
-fun ProtectedRegion.toRegionOrNull(): Region? = WorldEditRegionConverter.convertToRegion(this)
-
-/**
- * Converts this `ProtectedRegion` into a `Region`.
- *
- * @return The converted `Region` instance.
- * @throws IllegalStateException if the conversion fails.
- */
-fun ProtectedRegion.toRegion(): Region =
-    toRegionOrNull() ?: error("Failed to convert ProtectedRegion to Region: $this")
-
-/**
  * Calculates the approximate center point of a protected region.
  *
  * This method computes the center by adding the minimum and maximum points
@@ -62,17 +37,6 @@ fun ProtectedRegion.toRegion(): Region =
  * @return the center point of the region as a `BlockVector3`
  */
 fun ProtectedRegion.fastCenter(): BlockVector3 = minimumPoint.add(maximumPoint).divide(2)
-
-/**
- * Converts this 2D vector into a block position in a 3D space by truncating its
- * x and z components to integers using the floor function.
- * The y-coordinate can be optionally specified; its default value is 0.
- *
- * @param y The y-coordinate of the resulting block position. Defaults to 0 if not provided.
- * @return A block position based on this 2D vector and the given y-coordinate.
- */
-fun Vector2.toBlockPosition(y: Int = 0) =
-    Position.block(x().fastFloorToInt(), y, z().fastFloorToInt())
 
 /**
  * Converts a 2D block vector to a 3D block position by adding an optional Y-coordinate.
@@ -163,15 +127,6 @@ fun ProtectedRegion.getRegionManagerOrNull(): RegionManager? =
     regionContainer.loaded.find { it.hasRegion(id) }
 
 /**
- * Retrieves the associated RegionManager for the current ProtectedRegion.
- * Throws an exception if the RegionManager cannot be found.
- *
- * @return The RegionManager associated with this ProtectedRegion.
- */
-fun ProtectedRegion.getRegionManager(): RegionManager =
-    getRegionManagerOrNull() ?: error("Region manager not found for region: ${this.id}")
-
-/**
  * Calculates the volume of a protected region, which can either be flat (2D area) or include height (3D volume).
  *
  * @param flat Indicates whether to calculate a 2D flat area (true) or the 3D volume (false). Defaults to true.
@@ -195,25 +150,6 @@ fun ProtectedRegion.fixedVolume(flat: Boolean = true): Long {
     return if (flat) baseArea else {
         val height = (maximumPoint.y() - minimumPoint.y() + 1).toLong()
         baseArea * height
-    }
-}
-
-/**
- * Converts the string, typically representing a player's name, into a [LocalPlayer] object.
- *
- * This method checks if a player with the given name is currently online.
- * If the player is online, it wraps the result as a [LocalPlayer]. If the player
- * is offline, it obtains their offline representation and wraps it instead.
- *
- * @return A [LocalPlayer] instance for the given name, representing either
- * an online or offline player.
- */
-fun String.toLocalPlayer(): LocalPlayer {
-    val player = Bukkit.getPlayerExact(this)
-    return if (player != null && player.isOnline) {
-        WorldGuardPlugin.inst().wrapPlayer(player)
-    } else {
-        WorldGuardPlugin.inst().wrapOfflinePlayer(Bukkit.getOfflinePlayer(this))
     }
 }
 
@@ -287,18 +223,6 @@ val WorldEditLocation.world: WorldEditWorld
     get() = this.extent as? WorldEditWorld ?: error("Extent is not a World: $extent")
 
 /**
- * Converts a WorldEdit location to a Bukkit location.
- *
- * This method adapts the location from the WorldEdit API to the Bukkit API, ensuring compatibility
- * with Bukkit-based implementations.
- *
- * @return A Location instance representing the Bukkit equivalent of this WorldEdit location.
- */
-fun WorldEditLocation.toBukkitLocation(): Location {
-    return BukkitAdapter.adapt(this)
-}
-
-/**
  * Checks if the player is currently standing in the specified protected region.
  *
  * @param region The protected region to check against the player's current location.
@@ -307,39 +231,6 @@ fun WorldEditLocation.toBukkitLocation(): Location {
 fun Player.standsInProtectedRegion(region: ProtectedRegion) =
     location.getProtectedRegions().any { it.id == region.id }
 
-/**
- * Retrieves a list of member names associated with the protected region.
- *
- * This method processes the region's member unique IDs to obtain their corresponding names or unique IDs
- * as strings if the name is not available. The resulting list includes all member identifiers properly mapped.
- *
- * The process involves:
- * - Fetching members' unique IDs.
- * - Mapping them to offline player data using Bukkit's API.
- * - Returning their names, or unique ID strings if names are unavailable.
- *
- * @receiver The protected region from which member names are retrieved.
- * @return A list of member names or unique IDs as strings.
- */
-fun ProtectedRegion.getMemberNames() = members.uniqueIds.asSequence()
-    .map { Bukkit.getOfflinePlayer(it) }
-    .map { it.name ?: it.uniqueId.toString() }
-    .toObjectList()
-
-/**
- * Retrieves a list of owner names for the given protected region.
- *
- * This method processes the region's owner UUIDs, converts them to corresponding
- * offline players, and extracts their names. If a name is unavailable, the UUID
- * is used as a fallback representation.
- *
- * @receiver The protected region whose owner names are to be retrieved.
- * @return A list of strings representing the names of the owners.
- */
-fun ProtectedRegion.getOwnerNames() = owners.uniqueIds.asSequence()
-    .map { Bukkit.getOfflinePlayer(it) }
-    .map { it.name ?: it.uniqueId.toString() }
-    .toObjectList()
 
 /**
  * Retrieves a set of protected regions that the current location falls into.
@@ -358,36 +249,6 @@ fun Location.getProtectedRegions(withGlobalRegion: Boolean = false): Set<Protect
     } else {
         regions.regions.filterNotTo(mutableObjectSetOf()) { it.id == ProtectedRegion.GLOBAL_REGION }
     }
-}
-
-
-/**
- * Calculates the squared distance between this `BlockPosition` and another `BlockPosition`.
- *
- * @param other The other `BlockPosition` to calculate the squared distance to.
- * @return The squared distance as an `Int`.
- */
-fun BlockPosition.distanceSquared(other: BlockPosition): Int {
-    val dx = this.blockX() - other.blockX()
-    val dy = this.blockY() - other.blockY()
-    val dz = this.blockZ() - other.blockZ()
-    return dx * dx + dy * dy + dz * dz
-}
-
-/**
- * Calculates the squared Euclidean distance between this position and another position.
- *
- * This method computes the square of the distance to avoid the performance cost of
- * computing a square root when only relative distances are needed.
- *
- * @param other The other position to calculate the distance to.
- * @return The squared distance between this position and the given position.
- */
-fun Position.distanceSquared(other: Position): Double {
-    val dx = this.x() - other.x()
-    val dy = this.y() - other.y()
-    val dz = this.z() - other.z()
-    return dx * dx + dy * dy + dz * dz
 }
 
 /**
