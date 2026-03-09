ed45eddff0a0552d9e3c9cac96a24be2e47df793
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-03-09 16:28:53 +0100

    feat(protection): add predefined spawn locations for distance calculations

--- src/main/kotlin/dev/slne/surf/protect/paper/region/utils.kt
+++ src/main/kotlin/dev/slne/surf/protect/paper/region/utils.kt
@@ -1,14 +1,30 @@
 package dev.slne.surf.protect.paper.region
 
+import com.sk89q.worldedit.math.BlockVector3
 import com.sk89q.worldedit.util.Location
-import com.sk89q.worldedit.world.World
 import dev.slne.surf.protect.paper.config.config
 import dev.slne.surf.protect.paper.math.Mth
 
+private val spawns = listOf(
+    BlockVector3(0, 0, 0),
+    BlockVector3(0, 0, -25000),
+    BlockVector3(25000, 0, -25000),
+    BlockVector3(-25000, 0, -25000),
+    BlockVector3(0, 0, 25000),
+    BlockVector3(25000, 0, 25000),
+    BlockVector3(-25000, 0, 25000),
+    BlockVector3(25000, 0, 0),
+    BlockVector3(-25000, 0, 0)
+)
+
 fun Location.getProtectionPricePerBlock(): PricePerBlockResult {
-    val world = extent as World
-    val spawn = world.spawnPosition.withY(0)
-    val distance = toVector().toBlockPoint().withY(0).distance(spawn)
+    val pos = toVector().toBlockPoint().withY(0)
+
+    val nearestSpawn = spawns.minBy {
+        pos.distance(it)
+    }
+
+    val distance = pos.distance(nearestSpawn)
 
     if (distance < config.pricing.spawnProtectionPerBlock) {
         return PricePerBlockResult(Double.MAX_VALUE, distance)
