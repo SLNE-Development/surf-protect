0d9e15340edcb8581ae391a086302918dfc8943e
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-03-14 12:40:28 +0100

    fix(region): update placeholder name from "Wildnis" to "Wilderness"

--- src/main/kotlin/dev/slne/surf/protect/paper/papi/placeholder/CurrentRegionPlaceholder.kt
+++ src/main/kotlin/dev/slne/surf/protect/paper/papi/placeholder/CurrentRegionPlaceholder.kt
@@ -14,7 +14,7 @@ object CurrentRegionPlaceholder : PapiPlaceholder("current-region") {
         val region = player.location.getProtectedRegions(false).firstOrNull()
 
         return if (region == null) {
-            "Wildnis"
+            "Wilderness"
         } else {
             RegionInfo(region).name
         }
