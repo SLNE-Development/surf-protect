3552614c832d3a68ca225e3f34d4a0ba25bfa864
Keviro <25409956+Keviro@users.noreply.github.com>
2026-04-25 00:43:28 +0200

    ♻️ refactor(messages): remove unused imports from Messages.kt

--- src/main/kotlin/dev/slne/surf/protect/paper/message/Messages.kt
+++ src/main/kotlin/dev/slne/surf/protect/paper/message/Messages.kt
@@ -6,8 +6,6 @@ import dev.slne.surf.protect.paper.config
 import dev.slne.surf.protect.paper.region.info.RegionInfo
 import dev.slne.surf.protect.paper.util.blockFormat
 import dev.slne.surf.protect.paper.util.castCoinFormat
-import dev.slne.surf.surfapi.core.api.messages.Colors
-import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
 import dev.slne.surf.transaction.api.currency.Currency
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextDecoration
