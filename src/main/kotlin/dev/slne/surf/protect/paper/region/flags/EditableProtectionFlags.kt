245b9214a1b62dc44f2a32d682a134a36204ca41
JMKohnagel <jmkohnagel@gmail.com>
2026-05-12 22:51:13 +0200

    fix: block gravity default true

--- src/main/kotlin/dev/slne/surf/protect/paper/region/flags/EditableProtectionFlags.kt
+++ src/main/kotlin/dev/slne/surf/protect/paper/region/flags/EditableProtectionFlags.kt
@@ -233,7 +233,7 @@ enum class EditableProtectionFlags(
         displayName = "Blockgravitation",
         description = "Legt fest, ob Blöcke auf dem Grundstück durch Gravitation fallen dürfen.",
         flag = ProtectionFlagsRegistry.SURF_BLOCK_GRAVITY,
-        initialState = true
+        initialState = StateFlag.State.ALLOW
     );
 
     val displayNameComponent = SurfComponentBuilder { primary(displayName) }
