953b6b22e77fd73a734557613276b4f8e10af8f7
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-05-29 16:00:01 +0200

    feat: update Gradle version to 3.2.4 and simplify event cancellation logic in RegionListener

--- src/main/kotlin/dev/slne/surf/protect/paper/listener/listeners/RegionListener.kt
+++ src/main/kotlin/dev/slne/surf/protect/paper/listener/listeners/RegionListener.kt
@@ -57,7 +57,7 @@ object RegionListener : Listener {
         if (!Tag.CONCRETE_POWDER.isTagged(block.type)) return
 
         if (block.location.isGlobalRegion()) {
-            return  // Allow
+            return
         }
 
         val regions = block.location.getProtectedRegions()
@@ -122,9 +122,7 @@ object RegionListener : Listener {
             protection.members.contains(ownerUuid) ||
                     protection.owners.contains(ownerUuid)
 
-        if (!allowed) {
-            event.isCancelled = true
-        }
+        event.isCancelled = allowed
     }
 
     private fun gravityDeniedAt(location: Location) = location.getProtectedRegions().any {
