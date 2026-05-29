946ddb7a94600831b48e88d452018024681ba834
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-05-29 16:28:50 +0200

    feat: update explosion listener to handle TNT entity explosions and improve region protection

--- src/main/kotlin/dev/slne/surf/protect/paper/listener/listeners/ProtectionExplosionListener.kt
+++ src/main/kotlin/dev/slne/surf/protect/paper/listener/listeners/ProtectionExplosionListener.kt
@@ -3,21 +3,20 @@ package dev.slne.surf.protect.paper.listener.listeners
 import com.sk89q.worldguard.protection.flags.StateFlag
 import dev.slne.surf.protect.paper.region.flags.ProtectionFlagsRegistry
 import dev.slne.surf.protect.paper.util.getProtectedRegions
-import org.bukkit.Material
+import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
-import org.bukkit.event.block.BlockExplodeEvent
+import org.bukkit.event.entity.EntityExplodeEvent
 
 object ProtectionExplosionListener : Listener {
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
-    fun onBlockExplode(event: BlockExplodeEvent) {
-        if (event.explodedBlockState.type != Material.TNT) {
+    fun onBlockExplode(event: EntityExplodeEvent) {
+        if (event.entityType != EntityType.TNT) {
             return
         }
 
-        val block = event.block
-        val location = block.location
+        val location = event.location
         val regions = location.getProtectedRegions(false)
 
         if (regions.isEmpty()) {
