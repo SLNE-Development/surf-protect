89caddaaea79a0d112e76483a1328c70f4c78131
twisti-dev <76837088+twisti-dev@users.noreply.github.com>
2026-06-03 12:16:54 +0200

    ♻️ refactor(protection): exclude BEDROCK from bed material checks
    - update material checks to ensure BEDROCK is not included in bed-related protections

--- src/main/kotlin/dev/slne/surf/protect/paper/listener/listeners/ProtectionModeListener.kt
+++ src/main/kotlin/dev/slne/surf/protect/paper/listener/listeners/ProtectionModeListener.kt
@@ -196,7 +196,7 @@ object ProtectionModeListener : Listener {
         .contains("SHULKER_BOX")
         .contains("BARREL")
         .contains("BEACON")
-        .contains("BED")
+        .contains("BED").not(Material.BEDROCK) // BEDROCK is not a bed
         .contains("CAULDRON")
         .contains("COMPARATOR")
         .contains("DAYLIGHT_DETECTOR")
